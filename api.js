0e140f15c0d94328ab72485bd4f02f1293901e39
ATRodr <72571049+ATRodr@users.noreply.github.com>
2024-06-23 18:51:32 -0400

    Basic login without JWT added

--- api.js
+++ api.js
@@ -3,5 +3,41 @@ require('mongodb');
 
 exports.setApp = function(app, client)
 {
-    
+    // Login Incoming: 
+    // Username : ""
+    // Password : ""
+
+    // Login Outgoing
+    // FirstName : ""
+    // LastName : ""
+    // Email : ""
+    app.post('/api/login', async (req, res, next) =>
+    {
+        // Get username and password from request body
+        const {username, password} = req.body
+
+        // Check database's users collection to see if there is a matching login and password
+        const db = client.db();
+        const results = await db.collection('Users').find({Username: username, Password: password}).toArray();
+
+        // Initialize outgoing info
+        var firstname = "";
+        var lastname = "";
+        var email = "";
+
+        var ret;
+        // If matching user is found populate outgoing info
+        if (results.length > 0)
+        {
+            firstname = results[0].FirstName;
+            lastname = results[0].LastName;
+            email = results[0].Email;
+        }
+        else
+        {
+            ret = {error: "Username or password is incorrect!"};
+        }
+        var ret = {firstname:firstname, lastname:lastname, email:email};
+        res.status(200).json(ret);
+    });
 }
