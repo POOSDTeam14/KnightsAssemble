6768efc171c12cc8cb95e5d2228f5a6d67826e35
ATRodr <72571049+ATRodr@users.noreply.github.com>
2024-06-23 20:35:52 -0400

    Basic JWT setup

--- createJWT.js
+++ createJWT.js
@@ -1,2 +1,11 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
+const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+function createAccessToken(username)
+{
+    return jwt.sign({username: username}, ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+}
+
+module.exports = createAccessToken;
