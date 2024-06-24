d7187558b97d91e465b6398cbe990450f5177f55
ATRodr <72571049+ATRodr@users.noreply.github.com>
2024-06-23 20:53:11 -0400

    Update createJWT.js

--- createJWT.js
+++ createJWT.js
@@ -1,11 +1,8 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-
-function createAccessToken(username)
-{
-    return jwt.sign({username: username}, ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+function createAccessToken(username) {
+    return jwt.sign({ username: username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 }
 
 module.exports = createAccessToken;
