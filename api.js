d96ef85fa63a75c250322b056feb1b4102d7da06
ATRodr <72571049+ATRodr@users.noreply.github.com>
2024-06-29 11:11:08 -0400

    add createEvent

--- api.js
+++ api.js
@@ -71,45 +71,81 @@ exports.setApp = function(app, client)
     // LastName : ""
     // Email : ""
     app.post('/api/register', async (req, res, next) =>
+    {
+        // Get username,password, firstname, lastname, and email from request body
+        const {username, password, firstname, lastname, email} = req.body
+
+        if (!username | !password | !firstname | !lastname | !email)
         {
-            // Get username,password, firstname, lastname, and email from request body
-            const {username, password, firstname, lastname, email} = req.body
+            return res.status(400).json({error: "All fields must be filled out!"});
+        }
 
-            if (!username | !password | !firstname | !lastname | !email)
-            {
-                return res.status(400).json({error: "All fields must be filled out!"});
-            }
+        // Check database's users collection to see if there is an existing username
+        const db = client.db('KnightsAssembleDatabase');
+        const results = await db.collection('Users').find({Username: username}).toArray();
 
-            // Check database's users collection to see if there is an existing username
-            const db = client.db('KnightsAssembleDatabase');
-            const results = await db.collection('Users').find({Username: username}).toArray();
-
-            // User name already exists
-            if (results.length > 0)
-            {
-                return res.status(400).json({error: "Username already exists!"});
-            }           
+        // User name already exists
+        if (results.length > 0)
+        {
+            return res.status(400).json({error: "Username already exists!"});
+        }           
              
-            const newUser = {
-                Username: username,
-                Password: password,
-                FirstName: firstname,
-                LastName: lastname,
-                Email: email
-            };
+        const newUser = {
+            Username: username,
+            Password: password,
+            FirstName: firstname,
+            LastName: lastname,
+            Email: email
+        };
             
-            try 
-            {
-                var ret = await db.collection('Users').insertOne(newUser);
-                res.status(200).json(ret);
-            } 
-            catch (error) 
-            {
-                res.status(500).json({error: "User not registered"});
-            }
-            // Possibly todo: create JWT for the user as soon as they register
-            
-        });
+        try 
+        {
+            var ret = await db.collection('Users').insertOne(newUser);
+            res.status(200).json(ret);
+        } 
+        catch (error) 
+        {
+            res.status(500).json({error: "User not registered"});
+        }   
+    });
 
+    // Create Incoming: 
+    // Name : ""
+    // Type : ""
+    // Description : ""
+    // Time : Date
+    // Location : ""
+    // Capacity : Int32
+    // Attendees : Array
+    app.post('/api/createEvent', async (req, res, next) =>
+    {
+        // Get name,type, description, time, location, and capacity from request body
+        const {name, type, description, time, location, capacity} = req.body
 
+        if (!name | !type | !description | !time | !location | !capacity)
+        {
+            return res.status(400).json({error: "All fields must be filled out!"});
+        }
+
+        // Possibly do some check for identical events?
+
+        const newEvent = {
+            Name: name,
+            Type: type,
+            Description: description,
+            Time: time,
+            Location: location,
+            Capacity : capacity
+        };
+
+        try 
+        {
+            var ret = await db.collection('Events').insertOne(newEvent);
+            res.status(200).json(ret);
+        } 
+        catch (error) 
+        {
+            res.status(500).json({error: "Event not created"});
+        }
+    });
 }
