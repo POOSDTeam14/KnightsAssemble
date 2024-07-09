90b650b8f3ddfe740bd02b2d356b654a24a542dd
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-07-09 09:46:49 -0400

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import ForgetPassword from './ForgetPassword.js';
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+import jwt_decode from 'jwt-decode';
 const { storeToken } = require('../tokenStorage.js');
 
 function Login()
@@ -28,7 +27,7 @@ function Login()
 
               let res = JSON.parse(await response.text());
               const {accessToken} = res;
-              const decoded = jwt.decode(accessToken, {complete:true});
+              const decoded = jwt_decode(accessToken, {complete:true});
 
               let userInfo = decoded.UserInfo;
               let userId = userInfo.userid;
