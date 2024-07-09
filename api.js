092258161b15a5445fc02abbc86e01c88b89923b
EC0GAME <65595888+EC0GAME@users.noreply.github.com>
2024-07-09 13:17:26 -0400

    start findCreatedEvents

--- api.js
+++ api.js
@@ -218,6 +218,57 @@ exports.setApp = function(app, client)
         res.status(200).json({ret, token: newToken});
     });
 
+    app.post('/api/findCreatedEvents', async (req, res, next) =>
+    {
+        // Get eventid to add attendees to it
+        const {userid, token} = req.body
+
+        // Check for expired token
+        try 
+        {
+            if (isTokenExpired(token))
+            {
+                return res.status(401).json({error: "Your session is no longer valid"});
+            }
+        } 
+        catch (error) 
+        {
+            return res.status(401).json({error: "Something is wrong with your session"});
+        }
+        
+        const db = client.db('KnightsAssembleDatabase');
+        var eventResults = await db.collection('Events').find( { HostID: userid } ).toArray();
+        
+        // Look for user in all events, return to array if found
+        if ( eventResults.length>0 )
+            try
+            {
+                var ret = eventResults;
+            }
+            catch ( error )
+            {
+                return res.status(404).json({error: "Unable to find user in Events!"});
+            }
+        else
+        {
+            return res.status(405).json({error: "User is not in any current Events!"});
+        }
+        
+        // Refresh token at end of CRUD events
+        var newToken = null;
+        try 
+        {
+            newToken = refreshToken(token);
+        } 
+        catch (error) 
+        {
+            console.log(error);
+        }
+        
+        // Respond with event and token
+        res.status(200).json({ret, token: newToken});
+    });
+
     // Update Incoming: 
     // Name : ""
     // Type : ""
