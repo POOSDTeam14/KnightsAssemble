c6c4408553e7bc678fb2693ad6970df6027f3182
EC0GAME <65595888+EC0GAME@users.noreply.github.com>
2024-07-18 19:05:57 -0400

    Update api.js

--- api.js
+++ api.js
@@ -538,7 +538,7 @@ exports.setApp = function(app, client)
 
         var finalResults = [];
         searchResults.forEach( user => {
-            if ( (user.Type===typeFilter && typeFilter!=="") && (user.Time>dateFilter && dateFilter>datePreset) )
+            if ( (user.Type===typeFilter && typeFilter!==""))
             {
                 try
                 {
