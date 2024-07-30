e1a89ed64907fa7cf9eaeb5f8af3224bd9137de3
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-07-30 04:05:01 -0400

    ui changes

--- frontend/src/components/VerticalTabColumn.js
+++ frontend/src/components/VerticalTabColumn.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react';
 
-function VerticalTabColumn(){
-    return(
-        <div class="row g-0 verticalTabRow">
-            <div class="col h-100">
-                <div class = "verticalTabContent">
+function VerticalTabColumn() {
+    return (
+        <div className="row g-0 verticalTabRow">
+            <div className="col h-100">
+                <div className="verticalTabContent">
                     <a href="/events">
                         <p>Events</p>
                     </a>
                     <a href="/myevents">
                         <p>My Events</p>
                     </a>
-                    <a href = "/createevent">
+                    <a href="/createevent">
                         <p>Create Event</p>
                     </a>
                 </div>
