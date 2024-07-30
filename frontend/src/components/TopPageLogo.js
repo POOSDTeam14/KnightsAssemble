dd7c675d3cbc17af5f819e9d3fa843d6cc5b2ae0
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-07-30 03:45:55 -0400

    Banner changes

--- frontend/src/components/TopPageLogo.js
+++ frontend/src/components/TopPageLogo.js
@@ -8,21 +8,18 @@ function TopPageLogo() {
     window.location.href = "/login";
   }
 
-
   return (
-    <div className = "row g-0 upperBox">
-
-      <div className = "col-5 topLeft-Column">
-          <a href="https://github.com/POOSDTeam14/KnightsAssemble">
-            <img src="https://i.imgur.com/IhXxVvE.png" alt="UCF Knight Logo" className="img-fluid" />
-          </a>
-          <p className="mb-0">Knights Assemble</p>
-      </div> 
-
-      <div className = "col-7 topRight-Column">
-        {window.location.pathname === "/login" || window.location.pathname === "/register" || window.location.pathname === "/" ? null : <button className="logout-Button" onClick={logoutClicked}>LOGOUT</button>}
+    <div className="row g-0 upperBox">
+      <div className="col-5 col-md-4 col-lg-3 topLeft-Column">
+        <a href="https://github.com/POOSDTeam14/KnightsAssemble">
+          <img src="https://i.imgur.com/IhXxVvE.png" alt="UCF Knight Logo" className="img-fluid" />
+        </a>
+        <p className="mb-0">Knights Assemble</p>
       </div>
 
+      <div className="col-7 col-md-8 col-lg-9 topRight-Column">
+        {window.location.pathname === "/login" || window.location.pathname === "/register" || window.location.pathname === "/" ? null : <button className="logout-Button" onClick={logoutClicked}>Logout</button>}
+      </div>
     </div>
   );
 }
